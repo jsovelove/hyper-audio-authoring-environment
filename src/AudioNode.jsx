aed58582a0dbcb94d2766597f86e11b1fa8d496b
Jack Sovelove <soveloj@wwu.edu>
2023-11-03 17:23:19 -0700

    pause play button

--- src/AudioNode.jsx
+++ src/AudioNode.jsx
@@ -7,13 +7,14 @@ const CustomAudioNode = ({ data, isConnectable }) => {
   const [isPlaying, setIsPlaying] = useState(false);
   const waveformRef = useRef(null);
   const wavesurfer = useRef(null);
+  
  
   useEffect(() => {
     wavesurfer.current = WaveSurfer.create({
       container: waveformRef.current,
       waveColor: 'violet',
       progressColor: 'purple',
-      barWidth: 1
+      barWidth: 3
     });
 
     if (audioSource) {
@@ -53,7 +54,8 @@ const CustomAudioNode = ({ data, isConnectable }) => {
         isConnectable={isConnectable}
       />
       <button className="audio-control-button" onClick={handlePlayPause}>
-        {isPlaying ? 'Pause' : 'Play'}
+        {isPlaying ? <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M520-200v-560h240v560H520Zm-320 0v-560h240v560H200Zm400-80h80v-400h-80v400Zm-320 0h80v-400h-80v400Zm0-400v400-400Zm320 0v400-400Z"/></svg>: 
+        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M320-200v-560l440 280-440 280Zm80-280Zm0 134 210-134-210-134v268Z"/></svg>}
       </button>
       <div className="waveform-container" ref={waveformRef} />
       {audioAttributes && audioAttributes.map(attr => (
